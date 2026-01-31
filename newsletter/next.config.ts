103a872d0a4f8793113d37fe2af81957fac3baa6
MIgnac <mignac71@gmail.com>
2026-01-31 13:05:30 +0100

    Fixing 404 on Newsletter Dashboard

--- newsletter/next.config.ts
+++ newsletter/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/newsletter",
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
